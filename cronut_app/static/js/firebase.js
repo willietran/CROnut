d353487e4861254bcf9021be09fb40f071c4326c
Willie Tran <tran.william26@gmail.com>
2014-07-25 14:16:25 -0700

    Got the Google Login to work (basic)

--- cronut_app/static/js/firebase.js
+++ cronut_app/static/js/firebase.js
@@ -1,14 +1,8 @@
 /**
  * Created by WillieTran on 7/24/14.
  */
+
 $(document).ready(function() {
-
-    var fireJSON = new Firebase('https://cronut.firebaseio.com');
-    var auth = new FirebaseSimpleLogin(fireJSON, function(error, user) {
-        ...
-    });
-
-    auth.login('google');
 
 });
 
